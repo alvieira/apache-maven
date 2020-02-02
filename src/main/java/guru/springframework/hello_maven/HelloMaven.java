2b3ae9a4631e02ef2e0143f2053ccc04e7cdb36f
Aloisio Vieira <aloisio.vieira@gmail.com>
2020-02-02 18:54:14 -0300

    initial hello-maven project

---
+++ src/main/java/guru/springframework/hello_maven/HelloMaven.java
@@ -0,0 +1,12 @@
+package guru.springframework.hello_maven;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class HelloMaven {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Maven!!");
+		System.out.println(StringUtils.upperCase("hello maven!!"));
+	}
+
+}
---
+++ src/test/java/guru/springframework/hello_maven/HelloMavenTest.java
@@ -0,0 +1,15 @@
+package guru.springframework.hello_maven;
+
+import junit.framework.TestCase;
+
+public class HelloMavenTest extends TestCase {
+	
+	public void testHello() {
+		System.out.println("TEST - Hello...");
+	}
+	
+	public void testMaven() {
+		System.out.println("TEST - Maven!!!");
+	}
+
+}
